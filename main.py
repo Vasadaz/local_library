e1259d530dbcc32cae0f1a5c77bced70cecf58de
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-08-14 18:26:14 +0300

    Downloaded 10 books from tululu.org .

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import downloader
 
+
 LIBRARY_PATH_DIR = 'books'
 
+
 if __name__ == '__main__':
-    url = 'https://tululu.org/txt.php?id=32168'
-    downloader.download_file(url, LIBRARY_PATH_DIR)
+    for book_id in range(1, 11):
+        params = {'id': book_id}
+        url = 'https://tululu.org/txt.php'
+        save_path = downloader.download_file(LIBRARY_PATH_DIR, url, params)
+        print(save_path)
