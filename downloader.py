d7cceeb76d3ee0cdd2a6bd3d2b29d135e1459656
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-08-14 18:19:55 +0300

    Updated the 'download_file' function

--- downloader.py
+++ downloader.py
@@ -4,17 +4,20 @@ from pathlib import Path
 import requests
 
 
-def download_file(file_url: str, dir_name: str):
+def download_file(dir_name: str, file_url: str, params: dict = None) -> str:
     Path(dir_name).mkdir(parents=True, exist_ok=True)
-    file_name = get_file_full_name(file_url)
 
-    response = requests.get(file_url)
+    response = requests.get(file_url, params=params)
     response.raise_for_status()
 
+    file_name = get_file_full_name(response.url)
     file_save_path = f'{dir_name}/{file_name}'
 
-    with open(file_save_path, 'wb') as file:
-        file.write(response.content)
+    try:
+        with open(file_save_path, 'wb+') as file:
+            file.write(response.content)
+    except PermissionError:  # Если такой книги нет
+        return (f'Book {params["id"]} Not Found')
 
     return file_save_path
 
