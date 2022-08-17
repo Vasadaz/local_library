68a922851e015e7c4c632c35e0d828861dae5108
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-08-17 12:56:27 +0300

    Added the "parse_arg" function.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import requests
 
 import downloader
@@ -6,8 +7,33 @@ IMAGE_DIR_NAME = 'covers'
 LIBRARY_DIR_NAME = 'books'
 LIBRARY_URL = 'https://tululu.org/'
 
+
+def parse_arg() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description='Скрипт скачивает книги по их id с онлайн-библиотеки https://tululu.org  '
+                    '\nКниги сохраняются в директории ./books , а обложки к ним в ./covers '
+    )
+    parser.add_argument('-s',
+                        '--start_id',
+                        help='Начало диапазона id, по-умолчанию 1',
+                        default=1,
+                        type=int)
+    parser.add_argument('-e',
+                        '--end_id',
+                        help='Конец диапазона id, по-умолчанию 10',
+                        default=10,
+                        type=int)
+    parsed_args = parser.parse_args()
+
+    return parsed_args
+
+
 if __name__ == '__main__':
-    for book_id in range(1, 11):
+    args = parse_arg()
+    start_id = args.start_id
+    end_id = args.end_id + 1
+
+    for book_id in range(start_id, end_id):
         try:
             book_resources = downloader.parse_book_page(LIBRARY_URL, book_id)
             book_name = book_resources['book']
