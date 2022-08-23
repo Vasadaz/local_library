970abc53eec6210cc40cfe2d9c3b63c1ea5919c7
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-08-23 15:58:47 +0300

    Added new args

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,14 +1,21 @@
 import argparse
 import json
+import os
 import time
+
+from pathlib import Path
 from urllib.parse import urljoin
 
 import requests
 
 from bs4 import BeautifulSoup
 
-from main import get_library_notes
+import downloader
 
+
+IMAGE_DIR_NAME = 'covers'
+LIBRARY_DIR_NAME = 'books'
+LIBRARY_URL = 'https://tululu.org/'
 
 def get_last_page_num(response) -> int:
     soup = BeautifulSoup(response.text, 'lxml')
@@ -17,6 +24,41 @@ def get_last_page_num(response) -> int:
     last_page_num = int(last_page_num_tag.text)
 
     return last_page_num
+
+
+def get_book_resources(book_id: int,
+                       get_cover: bool = True,
+                       get_txt: bool = True,
+                       ) -> dict:
+    global IMAGE_DIR_NAME, LIBRARY_DIR_NAME, LIBRARY_URL
+
+    book_download_txt_url = urljoin(LIBRARY_URL, f'txt.php')
+    book_download_txt_url_params = {'id': book_id}
+    book_page_url = urljoin(LIBRARY_URL, f'b{book_id}/')
+
+    response = requests.get(book_page_url)
+    response.raise_for_status()
+
+    downloader.check_for_redirect(response)
+
+    library_notes = downloader.parse_library_notes(response, book_id)
+    book_name = library_notes['book']
+
+    if get_txt:
+        library_notes['book_path'] = downloader.download_txt(
+            book_download_txt_url,
+            LIBRARY_DIR_NAME,
+            book_name,
+            book_download_txt_url_params
+        )
+    if get_cover:
+        library_notes['cover_path'] = downloader.download_img(
+            library_notes['cover_url'],
+            IMAGE_DIR_NAME
+        )
+    del library_notes['cover_url']
+
+    return library_notes
 
 
 def parse_book_tags(response) -> list:
@@ -37,14 +79,34 @@ def parse_args() -> argparse.Namespace:
         '--start_page',
         help='Начало диапазона страниц, по-умолчанию 1',
         default=1,
-        type=int
+        type=int,
     )
     parser.add_argument(
         '-e',
         '--end_page',
         help='Конец диапазона страниц, по-умолчанию 4',
-        default=5,
-        type=int
+        type=int,
+    )
+    parser.add_argument(
+        '--dest_folder',
+        help='Указание пути к каталогу с результатами парсинга: картинкам, книгам, JSON.',
+        type=str,
+    )
+    parser.add_argument(
+        '--skip_imgs',
+        help='Скрипт не будет скачивать картинки',
+        action='store_true',
+    )
+    parser.add_argument(
+        '--skip_txt',
+        help='Скрипт не будет скачивать книги',
+        action='store_true',
+    )
+    parser.add_argument(
+        '--json_path',
+        help='Указание пути к JSON файлу с результатами.',
+        type=str,
+        default='./library_books.json'
     )
     parsed_args = parser.parse_args()
 
@@ -56,23 +118,32 @@ if __name__ == '__main__':
 
     args = parse_args()
     start_page = args.start_page
+    dest_folder = args.dest_folder
+    skip_imgs = args.skip_imgs
+    skip_txt = args.skip_txt
+    json_path = args.json_path
 
-    if args.end_page > start_page:
+    if args.end_page:
         end_page = args.end_page + 1
     else:
         response = requests.get(category_url)
         response.raise_for_status()
         end_page = get_last_page_num(response) + 1
 
+    if dest_folder:
+        Path(dest_folder).mkdir(parents=True, exist_ok=True)
+        IMAGE_DIR_NAME = os.path.join(dest_folder, IMAGE_DIR_NAME)
+        LIBRARY_DIR_NAME = os.path.join(dest_folder, LIBRARY_DIR_NAME)
+
     category_book_tags = []
     library_books = {}
 
     for page_num in range(start_page, end_page):
-        print(page_num)
         page_num_url = urljoin(category_url, str(page_num))
+
         response = requests.get(page_num_url)
         response.raise_for_status()
-        get_last_page_num(response)
+
         book_tags = parse_book_tags(response)
         category_book_tags.extend(book_tags)
 
@@ -81,7 +152,7 @@ if __name__ == '__main__':
             book_id = tag['href'].strip('/b')
 
             try:
-                book_notes = get_library_notes(book_id)
+                book_notes = get_book_resources(book_id)
                 library_books[book_id] = book_notes
                 break
             except requests.exceptions.HTTPError as err:
@@ -95,5 +166,5 @@ if __name__ == '__main__':
 
     library_json = json.dumps(library_books)
 
-    with open("library_books.json", "w+", encoding='utf8') as file:
+    with open(json_path, 'w+', encoding='utf8') as file:
         file.write(library_json)
