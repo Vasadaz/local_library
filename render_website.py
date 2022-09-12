a3e055d10e0c784c8ef36ff868d61a852df85daa
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-09-12 22:33:25 +0300

    Optimized the "reload_template" function

--- render_website.py
+++ render_website.py
@@ -1,8 +1,9 @@
 import json
 
 from pathlib import Path
+from urllib.parse import quote
 
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader, select_autoescape, filters
 from livereload import Server
 from more_itertools import chunked
 
@@ -11,19 +12,22 @@ def reload_template():
     with open('library_books.json', 'r', encoding="utf8") as file:
         books = json.load(file)
 
+    bookshelf_number = 10
+    books_on_bookshelf_number = 2
     pages_dir_name = 'pages'
     Path(pages_dir_name).mkdir(parents=True, exist_ok=True)
 
-    books_pages = list(chunked(books.values(), 10))
+    books_pages = list(chunked(books.values(), bookshelf_number))
 
     for page_num, page_books in enumerate(books_pages, 1):
-        books_rows = list(chunked(page_books, 2))
+        books_rows = list(chunked(page_books, books_on_bookshelf_number))
         template = env.get_template('template.html')
         rendered_page = template.render(
             books_rows=books_rows,
             page_last_num=len(books_pages),
             page_num=page_num,
         )
+        print(quote(books_rows[0][0]['book_path']))
 
         with open(f'{pages_dir_name}/index{page_num}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
@@ -32,7 +36,7 @@ def reload_template():
 if __name__ == '__main__':
     env = Environment(
         loader=FileSystemLoader('templates'),
-        autoescape=select_autoescape(['html', 'xml'])
+        autoescape=select_autoescape(['html', 'xml']),
     )
 
     reload_template()
